4c17f44b9832995a464fd15ae48aab79da1bc32e
Justin.Chan <Justin.Chan@peergroup.com>
2022-06-18 16:41:52 -0400

    added CRUD operations to staticdbconnector

---
+++ ChitterChatter/Constants/Models/UserConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants.Models
+{
+    public static class UserConstants
+    {
+        public static int USER_UNDEFINED => -1;
+        public static int USER_ADMIN => 1;
+
+        public static string TABLENAME => "users";
+        public static string PK_COL => "userID";
+        public static string USERNAME_COL => "userName";
+        public static string PASSWORD_COL => "password";
+        public static string ISADMIN_COL => "isAdmin";
+    }
+}
--- ChitterChatter/Server/Models/StaticDBConnector.cs
+++ ChitterChatter/Server/Models/StaticDBConnector.cs
@@ -7,6 +7,9 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using System.Configuration;
 
+using Constants.Models;
+using Constants.Controllers;
+
 namespace Server.Models
 {
     public static class StaticDBConnector
@@ -89,7 +92,95 @@ namespace Server.Models
 
         public static int CheckLoginCredentials(string enteredUserName, string enteredPassword)
         {
-            Int64 count = StaticDBConnector.GetCount()
+            Int64 count = StaticDBConnector.GetCount(UserConstants.TABLENAME, UserConstants.USERNAME_COL, string.Format("{0}={1}", UserConstants.USERNAME_COL, enteredUserName));
+            if (count != 1)
+            {
+                return ServerResponseCodes.USER_NOTRECOGNIZED;
+            }
+            string storedDBPassword = string.Empty;
+            try
+            {
+                storedDBPassword = StaticDBConnector.GetFieldFromEntry(UserConstants.TABLENAME, UserConstants.PASSWORD_COL, string.Format("{0}={1}", UserConstants.USERNAME_COL, enteredUserName)).ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (storedDBPassword != enteredPassword)
+            {
+                return ServerResponseCodes.INCORRECT_PASSWORD;
+            }
+
+            return ServerResponseCodes.LOGIN_SUCCESS;
+        }
+
+        public static void Create(string tableName, string[] columns, string[] values)
+        {
+            string columnQuery = string.Empty;
+            foreach (string column in columns)
+            {
+                columnQuery = columnQuery + column;
+                columnQuery = columnQuery + ",";
+            }
+            columnQuery = columnQuery.TrimEnd(',');
+
+            string valueQuery = string.Empty;
+            foreach (string value in values)
+            {
+                valueQuery = valueQuery + value;
+                valueQuery = valueQuery + ",";
+            }
+            valueQuery.TrimEnd(',');
+
+            string query = string.Format("INSERT INTO {0} ({1}) VALUES ({2})", tableName, columnQuery, valueQuery);
+
+            StaticDBConnector.command.CommandType = CommandType.Text;
+            StaticDBConnector.command.CommandText = query;
+
+            StaticDBConnector.command.ExecuteNonQuery();
+            return;
+        }
+
+        public static void Read(string tableName, string whereClause)
+        {
+
+            string query = string.Format("SELECT * FROM {1} WHERE {2};", tableName, whereClause;
+
+            StaticDBConnector.command.CommandType = CommandType.Text;
+            StaticDBConnector.command.CommandText = query;
+
+            StaticDBConnector.command.ExecuteNonQuery();
+            StaticDBConnector.adapter = new MySqlDataAdapter(StaticDBConnector.command);
+            try
+            {
+                StaticDBConnector.table.Clear();
+                StaticDBConnector.adapter.Fill(StaticDBConnector.table);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Update(string tableName, string whereClause, string updateClause)
+        {
+            string query = string.Format("UPDATE {0} SET {1} WHERE {2};", tableName, updateClause, whereClause);
+
+            StaticDBConnector.command.CommandType = CommandType.Text;
+            StaticDBConnector.command.CommandText = query;
+
+            StaticDBConnector.command.ExecuteNonQuery();
+        }
+
+        public static void Delete(string tableName, string whereClause)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}", tableName, whereClause);
+
+            StaticDBConnector.command.CommandType = CommandType.Text;
+            StaticDBConnector.command.CommandText = query;
+
+            StaticDBConnector.command.ExecuteNonQuery();
         }
     }
 }
