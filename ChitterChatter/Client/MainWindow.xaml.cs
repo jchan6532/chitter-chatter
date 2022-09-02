a4670f147475a6d1b7f2b89a6edd43c7032bebf8
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-02 10:14:30 -0400

    initialize new wpf project for UI

--- ChitterChatter/Client/MainWindow.xaml.cs
+++ ChitterChatter/Client/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
+using Client.ViewModels;
+using Client.Services;
 
 namespace Client
 {
@@ -20,9 +24,56 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            //try
+            //{
+            //    ViewModels.Client client = new ViewModels.Client();
+            //}
+            //catch (Exception e)
+            //{
+            //    LoggingService.Log(e.Message, "EXCEPTION");
+            //    MessageBox.Show(e.Message);
+            //    this.Close();
+            //}
         }
+
+        #endregion
+
+        #region On Loaded
+
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = drive;
+                item.Tag = drive;
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded;
+                this.FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
