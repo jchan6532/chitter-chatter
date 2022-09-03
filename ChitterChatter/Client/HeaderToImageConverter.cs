a483211f223382cee9f18f4984487e3665ed3ba4
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-02 20:37:15 -0400

    implemented header to image converter inside image source

--- ChitterChatter/Client/HeaderToImageConverter.cs
+++ ChitterChatter/Client/HeaderToImageConverter.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace Client
 {
     /// <summary>
     /// Converts full path to drive/folder/file image icons
     /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
     public class HeaderToImageConverter : IValueConverter
     {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return new object();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
