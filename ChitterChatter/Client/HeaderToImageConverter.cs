5bb84f95f2946232de20501a9f12a80ce70d05e8
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-05 23:37:07 -0400

    finished implementing header to image converter

--- ChitterChatter/Client/HeaderToImageConverter.cs
+++ ChitterChatter/Client/HeaderToImageConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -15,7 +16,26 @@ namespace Client
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new object();
+            // Get the full path
+            string path = (string)value;
+
+            // If the path is null, ignore
+            if (path == null)
+                return null;
+
+            // Get the name of the file/folder
+            string name = MainWindow.GetFileFolderName(path);
+
+            // By default, we presume an image
+            string image = "Images/file.png";
+
+            // If the name is blank, we presume it's a drive as we cannot have a blank file/folder name
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
