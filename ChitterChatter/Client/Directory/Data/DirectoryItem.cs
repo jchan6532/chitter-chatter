4985b37afad7c797c4d9d9e5f1461214d56ac733
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-07 14:27:19 -0400

    added models

---
+++ ChitterChatter/Client/Directory/Data/DirectoryItem.cs
@@ -0,0 +1,36 @@
+﻿using Client.Directory;
+
+namespace Client.Directory.Data
+{
+    /// <summary>
+    /// Information about a directory item such as a drive/file/folder
+    /// </summary>
+    public class DirectoryItem
+    {
+        /// <summary>
+        /// The type of this item
+        /// </summary>
+        public DirectoryItemType Type { get; set; }
+
+        /// <summary>
+        /// The absolute path to this item
+        /// </summary>
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// The name of this directory item
+        /// </summary>
+        public string Name { get
+            {
+                if (this.Type == DirectoryItemType.Drive)
+                {
+                    return this.FullPath;
+                }
+                else
+                {
+                    return DirectoryStructure.GetFileFolderName(this.FullPath);
+                }
+            }
+        }
+    }
+}
---
+++ ChitterChatter/Client/Directory/Data/DirectoryItemType.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Client.Directory.Data
+{
+    /// <summary>
+    /// The type of a directory item
+    /// </summary>
+    public enum DirectoryItemType
+    {
+        /// <summary>
+        /// A logical drive
+        /// </summary>
+        Drive,
+
+        /// <summary>
+        /// A physical file
+        /// </summary>
+        File,
+
+        /// <summary>
+        /// A folder
+        /// </summary>
+        Folder
+    }
+}
