6aa95a126027654257f00757bec20d6f2b30d29e
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-07 14:27:03 -0400

    added view models

---
+++ ChitterChatter/Client/Directory/ViewModels/DirectoryItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using Client.Directory.Data;
+
+namespace Client.Directory.ViewModels
+{
+    /// <summary>
+    /// View model for directory item
+    /// </summary>
+    public class DirectoryItemViewModel : BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DirectoryItem mItem;
+
+        public DirectoryItem Item { 
+            get 
+            { 
+                return this.mItem;
+            }
+            set 
+            {
+                this.mItem = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Item)));
+            }
+        }
+    }
+}
