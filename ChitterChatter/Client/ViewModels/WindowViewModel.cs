fa63f6cea2f89badfe5756f6083a4fead8a31a25
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2022-09-09 15:27:25 -0400

    initialized window view model

---
+++ ChitterChatter/Client/ViewModels/WindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TCPHelpers.BaseClasses.Client;
+
+namespace Client.ViewModels
+{
+    /// <summary>
+    /// The View Model for the custom flat window
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+
+    }
+}
